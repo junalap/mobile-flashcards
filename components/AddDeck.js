e138025985b019af33a97c7489a0864b8f20913b
Anuj Paliwal <anujpaliwal2.0@gmail.com>
2018-09-02 23:48:19 -0700

    Create new deck via text input and button press

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Text, TextInput, View, Button } from 'react-native'
+import { connect } from 'react-redux';
+import { addDeck } from '../actions/index';
 
-const AddDeck = () => (
-  <View>
-    <Text>
-      Add Deck View
-    </Text>
-  </View>
-)
+class AddDeck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Create Deck Title'}
+  }
+  render() {
+    return (
+      <View>
+        <Text>
+          Add Deck View
+        </Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Button
+          onPress={() => this.props.dispatch(addDeck(this.state.text))}
+          title='Create New Deck'
+        />
+      </View>
+    );
+  }
+}
 
-export default AddDeck;
+
+
+export default connect()(AddDeck);
